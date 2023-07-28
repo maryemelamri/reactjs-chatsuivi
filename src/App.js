0e0523fb92ef00ce88faf8c3452e3bf3d4bcae93
maryemelamri <meriammeri.mm@gmail.com>
2023-07-28 13:04:09 +0100

    react pj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {useState} from "react"
+import axios from "axios"; 
+function App(){
+  const [prompt, setPrompt] = useState("");  
+  const [response, setResponse] = useState("");
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+  axios.post("/chat",{prompt})
+  .then((res)=>{setResponse(res.data);})
+  .catch((err)=>{
+    console.error(err);
+  });
+};
+  return(
+    <>
+    <div >
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+          ask something
+          </label>
+        </div>
+        <div>
+          <input value={prompt} onChange={(e) =>setPrompt(e.target.value)}>
+          </input>
+        </div>
+        <div>
+          <button type="submit">
+          submit
+          </button>
+        </div>
+      </form>
+      <div>
+        <p>{response}</p>
+      </div>
     </div>
-  );
+    </>
+  )
 }
-
 export default App;
